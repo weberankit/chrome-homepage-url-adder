0a4de74b608a3c5610de046b994fa7f7e1444632
Ankit <codingank@gmail.com>
2023-03-15 23:07:15 +0530

    added delete

--- chrome.js
+++ chrome.js
@@ -20,6 +20,7 @@ const saveBtn=popupPage.querySelector(".url-save-btn")
 console.log(saveBtn)
 let count=1;
 saveBtn.addEventListener("click",()=>{
+ // window.open('https://www.w3schools.com/jsref/met_html_click.asp')
  let siteName=popupPage.querySelector(".site-Name").value  
  const siteUrl=popupPage.querySelector(".site-Url").value
  const containerList=document.querySelector(".list")
@@ -41,8 +42,9 @@ containerList.innerHTML=basket.map((item,indexId)=>{
  return (`
  <div class="container-site-detail">
   <p class="vistsite"> ${item.siteName} </p>
+  <p class="vistsite"> ${item.siteUrl} </p>
  <p class="edit" data-id=${indexId}>  Edit </p>
-<p class="remove">  remove </p>
+<p class="remove"  data-id=${count}>  remove </p>
 </div>
  `)
 }).join("")
@@ -51,6 +53,28 @@ containerList.innerHTML=basket.map((item,indexId)=>{
 const moreAction=[...containerList.querySelectorAll(".edit")]
 console.log(moreAction)
 const editName=containerList.querySelector(".edit")
+
+const removeItem=[...containerList.querySelectorAll(".remove")]
+removeItem.forEach((item)=>{
+  item.addEventListener("click",()=>{
+    console.log(item.dataset.id)
+  item.parentElement.remove();
+const  removeId=item.dataset.id;
+console.log(removeId,"i am removeID",typeof removeId)
+basket = basket.filter((item)=>{
+ console.log(typeof item.count)
+ return item.count != removeId
+
+
+})
+console.log(basket)
+
+  })
+})
+
+
+
+
 editName.addEventListener("click",()=>{
 
 })
@@ -63,7 +87,8 @@ moreAction.forEach((item,index)=>{
    changeSitename.innerHTML=`
    
    <div class="input-name-field">
-    <p> <input type="text" class="edit-input"></p>
+    <p> <input type="text" class="edit-input" ></p>
+    <p> <input type="text" class="edit-input-url"></p>
     <button class="save-site-name">Save</button>
    </div>
    
@@ -71,11 +96,12 @@ moreAction.forEach((item,index)=>{
   let updateSiteName=changeSitename.querySelector(".save-site-name")
   updateSiteName.addEventListener("click",()=>{
     const inputValue=changeSitename.querySelector(".edit-input").value
-      
+    const inputUrlValue=changeSitename.querySelector(".edit-input-url").value
     changeSitename.innerHTML=`
    
     <div class="input-name-field">
      <p> ${inputValue}</p>
+     <p> ${inputUrlValue}</p>
      <button data-unique="${count}" class="save-site-name">Save</button>
     </div>
     
@@ -110,5 +136,12 @@ moreAction.forEach((item,index)=>{
 
 
 
+document.getElementById("image-file").value
 
+let photo = document.getElementById("image-file").files[0]; 
+let formData = new FormData(); 
+
+formData.append("photo", photo); 
+fetch('/upload/image', {method: "POST", body: formData});
+console.log(formData)
 
